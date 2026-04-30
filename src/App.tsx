8454b1a78c19e784085e684cd6ad1533dbb27a58
Andry Marques <andry.marquesp@gmail.com>
2026-04-29 21:13:37 -0300

    fix erro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 // import heroImg from './assets/hero.png'
-import Logo from './assets/Galo.png'
+// import Logo from './assets/Galo.png'
 // import Logo from './assets/NaoExiste.png'   // CD
 import './App.css'
 
